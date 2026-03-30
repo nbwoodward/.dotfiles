5f79c82c8e5e8ef5cf1b0ce88e9d206225151c45
nbwoodward <nbwoodward@gmail.com>
2026-03-30 16:12:59 -0600

    update after M5 install

--- mklinks.sh
+++ mklinks.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/zsh
 
 #symlinks to go in home
-homefiles=(.tmux .tmux.conf .tmux.conf.local .tmux.conf.osx .tmux.conf.linux .vim .vimrc .vimrc.cmp .bashrc .zshrc.nick, .alacritty.toml)
+homefiles=(.tmux .tmux.conf .tmux.conf.local .tmux.conf.osx .tmux.conf.linux .vim .vimrc .vimrc.cmp .vimrc.treesitter .bashrc .zshrc.nick, .alacritty.toml)
 
 backup_dir=~/dev_bak
 
